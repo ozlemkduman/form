dd51bd14a897e25938c48b87c2982e32f0ffa60b
ozlemkduman <ozlemk.duman@gmail.com>
2023-01-19 21:19:13 +0300

    form işlemi diğer inputlarla tamamlandı

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-const inputEmail=document.querySelector(".inputEmail");
-const inputFullName=document.querySelector(".inputFullName");
-const inputPhone=document.querySelector(".inputPhone");
-const inputPassword=document.querySelector(".inputPassword");
-const inputPassword2=document.querySelector(".inputPassword2");
-const formMainArea=document.querySelector(".formMainArea");
+const inputEmail = document.querySelector(".inputEmail");
+const inputFullName = document.querySelector(".inputFullName");
+const inputPhone = document.querySelector(".inputPhone");
+const inputPassword = document.querySelector(".inputPassword");
+const inputPassword2 = document.querySelector(".inputPassword2");
+const textArea = document.querySelector(".textArea");
+const formMainArea = document.querySelector(".formMainArea");
 
-formMainArea.addEventListener("submit",function(){
-    console.log("kes");
-    saveLocal();
-})
+formMainArea.addEventListener("submit", function () {
+  saveLocal();
+  console.log("ds");
+});
 
-function inputName(input){
-    let name=input.name;
-    return name;
+function inputName(input) {
+  let getName = input.name;
+  return getName;
 }
-function inputValue(input){
-    let value=input.value;
-    return value;
+function inputValue(input) {
+  let value = input.value;
+  return value;
 }
 
-function saveLocal(){
-    localStorage.setItem(inputName(inputEmail),JSON.stringify(inputValue(inputEmail)));
+
+function saveLocal() {
+  const setEmail = localStorage.setItem(
+    inputName(inputEmail),
+    JSON.stringify(inputValue(inputEmail))
+  );
+
+  const setFullname = localStorage.setItem(
+    inputName(inputFullName),
+    JSON.stringify(inputValue(inputFullName))
+  );
+
+  const setPhone = localStorage.setItem(
+    inputName(inputPhone),
+    JSON.stringify(inputValue(inputPhone))
+  );
+
+  const setPassword = localStorage.setItem(
+    inputName(inputPassword),
+    JSON.stringify(inputValue(inputPassword))
+  );
+
+  const setPassword2 = localStorage.setItem(
+    inputName(inputPassword2),
+    JSON.stringify(inputValue(inputPassword2))
+  );
+
+  const setTextArea = localStorage.setItem(
+    inputName(textArea),
+    JSON.stringify(inputValue(textArea))
+  );
 }
--- scriptform.js
+++ scriptform.js
@@ -1,9 +1,65 @@
-const userInfo= document.querySelector(".userInfo");
+const userInfo = document.querySelector(".userInfo");
 
-userInfo.textContent=`${loadLocal()} - mail adresiniz ile sisteme kaydınız oluşturulmuştur.`
+const liName=document.getElementById("liName");
+const liMail=document.getElementById("liMail");
+const liPhone=document.getElementById("liPhone");
+const liPassword=document.getElementById("liPassword");
+const liPassword2=document.getElementById("liPassword2");
+const liAddress=document.getElementById("liAddress");
 
-function loadLocal(){
-    const getLocal=localStorage.getItem("email");
-    const loadJson=JSON.parse(getLocal);
-    return loadJson;
+const list=document.querySelector(".list");
+
+
+
+
+function loadLocal() {
+  const getLocalEmail = localStorage.getItem("email");
+  const loadJsonEmail = JSON.parse(getLocalEmail);
+
+  const getLocalFullName = localStorage.getItem("fullName");
+  const loadJsonFullName = JSON.parse(getLocalFullName);
+
+  const getLocalPhone = localStorage.getItem("phoneNumber");
+  const loadJsonPhone = JSON.parse(getLocalPhone);
+
+  const getLocalPassword = localStorage.getItem("password");
+  const loadJsonPassword = JSON.parse(getLocalPassword);
+
+  const getLocalPassword2 = localStorage.getItem("password2");
+  const loadJsonPassword2 = JSON.parse(getLocalPassword2);
+
+  const getLocalAddress = localStorage.getItem("address");
+  const loadJsonAddress = JSON.parse(getLocalAddress);
+
+  let keyTotal= 
+    [
+    loadJsonFullName ,
+    loadJsonEmail ,
+    loadJsonPhone ,
+    loadJsonPassword ,
+    loadJsonPassword2 ,
+    loadJsonAddress]
+  ;
+  return keyTotal;
 }
+ 
+let keyLocal=loadLocal();
+
+function mainForm() {
+  
+  infoList();
+  userInfo.textContent=`Merhaba ${keyLocal[0]}  Kaydınız başarı ile alınmıştır. 101 masası kurulduğunda tarafınıza bilgi iletilecektir. Şeytanınız bol olsun dileriz... 
+  "Türkiye 101 oynayanlar federasyonu "`
+}
+
+function infoList(){
+    list.style.display="block"; 
+    liName.textContent=`İsim: ${keyLocal[0]}`;
+    liMail.textContent=`Mail: ${keyLocal[1]}`;
+    liPhone.textContent=`Telefon: ${keyLocal[2]}`;
+    liPassword.textContent=`Şifre: ${keyLocal[3]}`;
+    liPassword2.textContent=`Şifre Doğrulama: ${keyLocal[4]}`;
+    liAddress.textContent=`Adres: ${keyLocal[5]}`;
+}
+
+mainForm();
