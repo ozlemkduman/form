76b201881efba159fce7acd35a305cc8fcf7023a
ozlemkduman <ozlemk.duman@gmail.com>
2023-01-21 17:05:42 +0300

    value

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const inputPassword = document.querySelector(".inputPassword");
 const inputPassword2 = document.querySelector(".inputPassword2");
 const textArea = document.querySelector(".textArea");
 const formMainArea = document.querySelector(".formMainArea");
+const buttonLoad=document.querySelector(".buttonLoad");
+
 
 formMainArea.addEventListener("submit", function () {
+  
   saveLocal();
   console.log("ds");
 });
@@ -20,6 +23,52 @@ function inputValue(input) {
   return value;
 }
 
+//form sayfası load işlemi
+function loadLocal() {
+  const getLocalEmail = localStorage.getItem("email");
+  const loadJsonEmail = JSON.parse(getLocalEmail);
+
+  const getLocalFullName = localStorage.getItem("fullName");
+  const loadJsonFullName = JSON.parse(getLocalFullName);
+
+  const getLocalPhone = localStorage.getItem("phoneNumber");
+  const loadJsonPhone = JSON.parse(getLocalPhone);
+
+  const getLocalPassword = localStorage.getItem("password");
+  const loadJsonPassword = JSON.parse(getLocalPassword);
+
+  const getLocalPassword2 = localStorage.getItem("password2");
+  const loadJsonPassword2 = JSON.parse(getLocalPassword2);
+
+  const getLocalAddress = localStorage.getItem("address");
+  const loadJsonAddress = JSON.parse(getLocalAddress);
+
+  let keyTotal= 
+    [
+    loadJsonFullName ,
+    loadJsonEmail ,
+    loadJsonPhone ,
+    loadJsonPassword ,
+    loadJsonPassword2 ,
+    loadJsonAddress]
+  ;
+  return keyTotal;
+}
+let keyLocal=loadLocal();
+
+
+
+
+buttonLoad.addEventListener("click",function(){
+  
+  if (localStorage.getItem("email") === null) {
+    
+    console.log("hey");
+    
+
+  }
+
+})
 
 function saveLocal() {
   const setEmail = localStorage.setItem(
